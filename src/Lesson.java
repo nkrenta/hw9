d7ae6a0f33165d50e620aab9ed9661b54f5536f5
origin <nkrenta@inbox.ru>
2024-09-12 00:56:16 +0300

    hw9 is done!

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,5 +1,38 @@
+import java.util.Arrays;
+
 public class Lesson {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        int[] weightsCopy = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+
+        boolean arrayAreEqual = true;
+        arrayAreEqual = weights.length == weightsCopy.length;
+        if (arrayAreEqual) {
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] != weightsCopy[i]) {
+                    arrayAreEqual = false;
+                }
+            }
+        }
+        if (arrayAreEqual) {
+            System.out.println("Arrays are the same!");
+        } else {
+            System.out.println("Arrays are different!");
+            System.out.println("weightsCopy: " + Arrays.toString(weightsCopy));
+        }
+
+        int maxWeight = -1;
+        for (final int current : weights) {
+            if (current > maxWeight) {
+                maxWeight = current;
+            }
+        }
+        System.out.println(maxWeight);
+
+        for (int i = 0; i < weights.length - 1 && weights[i + 1] != 0; i++) {
+            System.out.println(weights[i + 1] - weights[i]);
+        }
+
     }
 }
